c72a27a451c7b9b627d9a64cc92c2fe30c901f0d
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2023-06-10 19:50:23 +0800

    Previews

---
+++ src/assets/js/main.js
@@ -0,0 +1,28 @@
+window.RGP = {};
+RGP.Current = {
+        ColorArray: ["#91e9c3", "#eaaa8e"],
+        LinearGradient: "linear-gradient(45deg, #91e9c3, #eaaa8e)"
+    };
+RGP.gen = function (ColorCount, degree) {
+
+    let ColorArray = [];
+    for (let i = 0; i < ColorCount; i++) {
+        ColorArray[i] = "#" + Math.floor( Math.random() * 0xffffff ).toString(16);
+    }
+
+    return {
+        ColorArray: ColorArray,
+        LinearGradient: `linear-gradient(${degree || 0}deg, ${ColorArray.toFormattedString()})`
+    };
+
+}
+
+RGP.run = function (ColorCount, degree) {
+    RGP.Current = RGP.gen(ColorCount, degree);
+
+    document.dispatchEvent(new CustomEvent("RGP_NewGradients"))
+}
+
+Array.prototype.toFormattedString = function () {
+    return this.toString().replace(/,/gi, "\, ")
+}
