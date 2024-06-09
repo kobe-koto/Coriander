a0daf7ed6225b7d9c568ccf7c1da7ca70888ad2d
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2024-06-09 21:04:49 +0800

    update depends

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,18 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import fs from "node:fs";
 import compress from "astro-compress";
-const {
-  homepage: domain
-} = JSON.parse(fs.readFileSync("./package.json"));
+import icon from "astro-icon";
+const { homepage: domain } = JSON.parse(fs.readFileSync("./package.json"));
 
 
 // https://astro.build/config
 export default defineConfig({
   site: domain,
-  integrations: [sitemap({
-    filter: page => page !== `${domain}/404.html` && page !== `${domain}/404/` && page !== `${domain}/404`
-  }), compress()]
+  integrations: [
+    sitemap({
+      filter: page => page !== `${domain}/404.html` && page !== `${domain}/404/` && page !== `${domain}/404`
+    }), 
+    compress(),
+    icon()
+  ]
 });
