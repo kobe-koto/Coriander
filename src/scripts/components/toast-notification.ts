4eea71f5bba77b510e17306c8fc539e7b0ba686d
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2026-02-18 22:21:48 +0800

    animation optimizes for toast notification

--- src/scripts/components/toast-notification.ts
+++ src/scripts/components/toast-notification.ts
@@ -2,15 +2,24 @@ class ToastNotification extends HTMLElement {
     constructor() {
         super();
     }
-    new(IconID: string, message: string) {
+    // Duration, milliseconds, from "PoppingIn" ends to "PoppingOut" starts
+    new(IconID: string, message: string, duration: number = 2000) {
         const ToastElement = this.querySelector(".prototype > .toast").cloneNode(true) as HTMLElement;
         const TargetIcon = this.querySelector(`.prototype > .symbols > [data-icon="${IconID}"]`).cloneNode(true);
         ToastElement.querySelector(".symbol").appendChild(TargetIcon);
         ToastElement.querySelector(".msg").textContent = message;
+        ToastElement.classList.add("PoppingIn");
         this.appendChild(ToastElement);
-        setTimeout(() => {
-            ToastElement.remove();
-        }, 2350)
+        ToastElement.addEventListener("animationend", () => {
+            ToastElement.classList.remove("PoppingIn");
+            setTimeout(() => {
+                ToastElement.classList.add("PoppingOut");
+                ToastElement.style.marginTop = `-${ToastElement.offsetHeight}px`;
+                ToastElement.addEventListener("animationend", () => {
+                    ToastElement.remove();
+                }, { once: true });
+            }, duration);
+        }, { once: true });
     }
 }
 customElements.define("toast-notification", ToastNotification);
